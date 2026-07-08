ca6c98ff939b009f48eb4a9ad2256a08567b74db
Pedro Dec <pcamposdec@gmail.com>
2026-07-08 14:08:34 +0000

    fix: fix tests

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -1,5 +1,6 @@
 import retry from "async-retry";
 import { faker } from "@faker-js/faker";
+
 import database from "infra/database.js";
 import migrator from "models/migrator.js";
 import user from "models/user.js";
